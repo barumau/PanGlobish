68091f0400c4edb1c107989b3b7532f4644112fb
Risto Kupsala <risto@pandunia.info>
2020-06-07 23:30:52 +0300

    uze di korekte adrese on edite of di GitHub paje

--- make_webe_paje.sh
+++ make_webe_paje.sh
@@ -1,8 +1,24 @@
-cd mkdocs
-cd english
-mkdocs build
-cd ../suomi
-mkdocs build
-cd ../tai
-mkdocs build
+make_di_MkDocs() {
+  cd mkdocs/$1
+  mkdocs build
+  cd ../..
+}
+
+replace_GitHub_folde() {
+    grep -rl "edit\/master\/docs" html/$1 | xargs sed -i "s/edit\/master\/docs/edit\/master\/$1/g"
+}
+
+make_di_webe_paje() {
+    echo $1
+    make_di_MkDocs $1
+    replace_GitHub_folde $1
+}
+
+make_ale() {
+    make_di_webe_paje english
+    make_di_webe_paje suomi
+    make_di_webe_paje tai
+}
+
+make_ale
 
